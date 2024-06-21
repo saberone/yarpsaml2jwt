fe73929409e9869b64417be1133457022ba9d2cf
Saber Karmous <saber.karmous@bngbank.nl>
2024-06-21 10:10:20 +0200

    Introduce ITokenIssuer

---
+++ src/Proxy/Infrastructure/ITokenIssuer.cs
@@ -0,0 +1,8 @@
+﻿using System.Security.Claims;
+
+namespace Proxy.Infrastructure;
+
+public interface ITokenIssuer
+{
+    string CreateToken(ClaimsIdentity identity, string? audience = null);
+}
--- src/Proxy/Infrastructure/TokenIssuer.cs
+++ src/Proxy/Infrastructure/TokenIssuer.cs
@@ -1,28 +1,24 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Proxy.Configuration;
 
 namespace Proxy.Infrastructure;
 
-public class TokenIssuer
+public class TokenIssuer : ITokenIssuer
 {
-    private readonly IOptions<TokenIssuerConfig> _options;
     private readonly TimeSpan _expiration = TimeSpan.FromHours(1);
     private readonly JwtSecurityTokenHandler _tokenHandler = new();
+    private SigningCredentials _credentials;
     public static string Audience => "Super.API";
     public static string Issuer => "Proxy";
-    public IList<SigningCredentials> SigningCredentials { get; } = new List<SigningCredentials>();
 
     public TokenIssuer(IOptions<TokenIssuerConfig> options)
     {
-        _options = options;
-        var securityKey = new SymmetricSecurityKey(System.Text.Encoding.Default.GetBytes(_options.Value.TokenSigningKey));
-        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
-        
-        SigningCredentials.Add(credentials);
+        var securityKey = new SymmetricSecurityKey(Encoding.Default.GetBytes(options.Value.TokenSigningKey));
+        _credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
     }
 
     public string CreateToken(ClaimsIdentity identity, string? audience = null)
@@ -33,15 +29,15 @@ public class TokenIssuer
         var tokenDescriptor = new SecurityTokenDescriptor
         {
             Audience = audience ?? Audience,
-            Expires = nowUtc.Add(this._expiration),
+            Expires = nowUtc.Add(_expiration),
             IssuedAt = issuedUtc,
             Issuer = Issuer,
             NotBefore = issuedUtc,
-            SigningCredentials = this.SigningCredentials.First(),
+            SigningCredentials = _credentials,
             Subject = identity
         };
 
-        var token = this._tokenHandler.CreateToken(tokenDescriptor);
-        return this._tokenHandler.WriteToken(token);
+        var token = _tokenHandler.CreateToken(tokenDescriptor);
+        return _tokenHandler.WriteToken(token);
     }
 }
